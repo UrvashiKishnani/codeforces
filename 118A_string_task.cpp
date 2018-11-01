b30aa35585cc36aa50ffd115200ee28f7f3fc5b8
UrvashiKishnani <41088003+UrvashiKishnani@users.noreply.github.com>
2018-11-01 10:38:08 +0400

    Update 118A_string_task.cpp

--- 118A_string_task.cpp
+++ 118A_string_task.cpp
@@ -8,12 +8,12 @@ int main() {
 	cin>>a;
 	n=a.size();
 	for(i=0;i<n;i++)
-		{
-			a[i]=tolower(a[i]);
-			if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='y')
-					continue;
-			else
-				cout<<"."<<a[i];
-		}	
+	{
+		a[i]=tolower(a[i]);
+		if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='y')
+			continue;
+		else
+			cout<<"."<<a[i];
+	}	
 	return 0;
 }
